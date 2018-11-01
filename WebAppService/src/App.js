2c25e091e2fa68e4da7f3893141ef19090c7e604
Jay Zuo <yjzuo6@gmail.com>
2018-10-31 20:35:57 -0400

    Revert "Display block view when event is clicked (#33)" (#34)
    This reverts commit 3361944210c751d14a3db182fbfe17206833f614.

--- WebAppService/src/App.js
+++ WebAppService/src/App.js
@@ -8,9 +8,6 @@ import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import logo from "./logo.svg";
 
-import Sidebar from "./Sidebar.js";
-import BlockView from "./BlockView.js";
-
 Calendar.setLocalizer(Calendar.momentLocalizer(moment));
 
 const DnDCalendar = withDragAndDrop(Calendar);
@@ -22,12 +19,7 @@ class App extends Component {
         start: new Date(),
         end: new Date(moment().add(1, "days")),
         title: "Test Event"
-      },
-      {
-        start: new Date(moment().add(1, "days")),
-        end: new Date(moment().add(2, "days")),
-        title: "Test Event"
-      },
+      }
     ]
   };
 
@@ -43,34 +35,25 @@ class App extends Component {
     console.log(start);
   };
 
-  onSelectEvent = (event, e) => {
-    this.refs.blockView.onSelectEvent(event);
-  };
-
   render() {
     return (
       <div className="App">
-				<div className="App-container">
-					<header className="App-header">
-						<img src={logo} className="App-logo" alt="logo" />
-						<h1 className="App-title">Welcome to React</h1>
-					</header>
-					<p className="App-intro">
-						To get started, edit <code>src/App.js</code> and save to reload.
-					</p>
-					<DnDCalendar
-						defaultDate={new Date()}
-						defaultView="month"
-						events={this.state.events}
-						onEventDrop={this.onEventDrop}
-            onEventResize={this.onEventResize}
-            onSelectEvent={this.onSelectEvent}
-						resizable
-						style={{ height: "100vh" }}
-					/>
-				</div>
-        <BlockView ref="blockView" />
-				<Sidebar/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <DnDCalendar
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          onEventDrop={this.onEventDrop}
+          onEventResize={this.onEventResize}
+          resizable
+          style={{ height: "100vh" }}
+        />
       </div>
     );
   }
