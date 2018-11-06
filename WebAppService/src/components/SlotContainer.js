452a16dd460b234391b6059d72b0c0319ab483a9
BC <bensonchan@users.noreply.github.com>
2018-11-06 13:04:23 -0500

    (#16) Add user roles, remake tests

--- WebAppService/src/components/SlotContainer.js
+++ WebAppService/src/components/SlotContainer.js
@@ -14,27 +14,46 @@ export default class SlotContainer extends React.Component {
 		this.setState({slots: slots});
 	}
 	
-	handleClick(event) {
+	handleSlotClick = (i) => (event) => {
+		if (this.state.slots[i].utorId == "") {
+			// Click on empty slot to assign
+			this.updateSlot(i, this.props.user, "");
+		} else {
+			if (this.props.user == this.state.slots[i].utorId) {
+				// Click on own slot to delete
+				this.updateSlot(i, "", "");
+			}
+			//Click on occupied slot to do nothing
+		}
 	}
 	
 	handleUtorIdChange = (i) => (event) => {
-		const newSlots = this.state.slots.slice();
-		newSlots[i].utorId = event.target.value;
-		newSlots[i].note = "";  // Delete note to protect privacy
-		this.setState({slots: newSlots});
+		// Delete note to protect privacy
+		this.updateSlot(i, event.target.value, "");
 	}
 	
 	handleNoteChange = (i) => (event) => {
+		// Can't edit note that belongs to nobody
+		if (this.state.slots[i].utorId !== "") {
+			this.updateSlot(i, this.state.slots[i].utorId, event.target.value);
+		}
+	}
+	
+	updateSlot(i, utorId, note) {
 		const newSlots = this.state.slots.slice();
-		newSlots[i].note = event.target.value;
+		newSlots[i].utorId = utorId;
+		newSlots[i].note = note;
 		this.setState({slots: newSlots});
 	}
 	
 	handleEmpty = () => {
 		const newSlots = this.state.slots.slice();
 		newSlots.map((slot, i) => {
-			newSlots[i].utorId = "";
-			newSlots[i].note = "";
+			if ((!this.props.isStudent) ||
+					(this.props.isStudent && this.props.user == newSlots[i].utorId)){
+				newSlots[i].utorId = "";
+				newSlots[i].note = "";
+			}
 		});
 		this.setState({slots: newSlots});
 	}
@@ -52,10 +71,27 @@ export default class SlotContainer extends React.Component {
 	renderSlots() {
 		return (
 			this.state.slots.map((slot, i) => 
-				<div className="slot" id={`slot${i}`} key={i}>
+				<div 
+					className="slot"
+					id={`slot${i}`}
+					key={i}
+					onClick={this.props.isStudent ? this.handleSlotClick(i) : undefined}
+				>
 					{moment(this.props.startTime + this.props.slotDuration * i).format("h:mmA - ")}
 					{moment(this.props.startTime + this.props.slotDuration * (i + 1)).format("h:mmA")}
-					<input
+					{this.props.isStudent ? (
+						<div id={`utorId${i}`}>
+						{(this.props.user == this.state.slots[i].utorId) ? (
+							this.props.user
+						) : (
+							this.state.slots[i].utorId == "" ? (
+								"Available"  // Hide utorId of other students
+							) : (
+								"Not Available"  // Hide utorId of other students
+							)
+						)}</div>
+					) : (
+						<input
 						className="text-input"
 						name={`utorId${i}`}
 						id={`utorId${i}`}
@@ -64,17 +100,22 @@ export default class SlotContainer extends React.Component {
 						placeholder="Unassigned UtorID..."
 						maxLength={50}
 						onChange={this.handleUtorIdChange(i)}
-					/>
-					<input
-						className="text-input"
-						name={`note${i}`}
-						id={`note${i}`}
-						type="text"
-						value={this.state.slots[i].note}
-						placeholder="Empty Note..."
-						maxLength={280}
-						onChange={this.handleNoteChange(i)}
-					/>
+						/>
+					)}
+					{(!this.props.isStudent || this.props.user == this.state.slots[i].utorId) ? (
+						<input
+							className="text-input"
+							name={`note${i}`}
+							id={`note${i}`}
+							type="text"
+							value={this.state.slots[i].note}
+							placeholder="Empty Note..."
+							maxLength={280}
+							onChange={this.handleNoteChange(i)}
+						/>
+					) : (
+						<div id={`note${i}`} />  // Hide notes that don't belong to student
+					)}
 				</div>
 			)
 		);
@@ -84,7 +125,11 @@ export default class SlotContainer extends React.Component {
 		return (
 			<div className="slot-container">
 				{this.renderSlots()}
-				<button id="empty-button" onClick={this.handleEmpty}>Empty All Slots</button>
+				{this.props.isStudent ? (
+					<button id="empty-button" onClick={this.handleEmpty}>Empty My Slots</button>
+				):(
+					<button id="empty-button" onClick={this.handleEmpty}>Empty All Slots</button>
+				)}
 				<button id="confirm-button" onClick={this.handleConfirm}>Confirm</button>
 				<button id="cancel-button" onClick={this.handleCancel}>Cancel</button>
 			</div>
--- WebAppService/src/tests/SlotContainer.test.js
+++ WebAppService/src/tests/SlotContainer.test.js
@@ -1,9 +1,125 @@
 import React from "react";
+import renderer from 'react-test-renderer';
 import {shallow, mount} from "enzyme";
 import moment from "moment";
 import SlotContainer from "../components/SlotContainer";
 
-let testProps, testSlots, mockChange, mockSubmit, mockClick;
+let testProps, testSlots;
+
+describe("instructor view", () => {
+	beforeEach(() => {
+		testSlots = [
+			{utorId: "parkerpeter15", note: "Everyone gets one."},
+			{utorId: "watsonmj25", note: "Face it Tiger... you just hit the jackpot!"},
+			{utorId: "", note: ""}
+		];
+		testProps = {
+			slots: testSlots.slice(),
+			startTime: moment("2077/01/01 00:00", "YYYY-MM-DD HH:mm"),
+			slotDuration: 600000,
+			isStudent: false,
+			user: "octaviusotto3"
+		};
+	});
+	
+	test("render", () => {
+		const tree = renderer.create(<SlotContainer {...testProps}/>).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+	
+	test("handleSlotClick can't be called", () => {
+		const wrapper = shallow(<SlotContainer {...testProps}/>);
+		expect(wrapper.find("#slot0").props().onClick).toEqual(undefined);
+	});
+	
+	test("handleUtorIdChange changes utorId", () => {
+		const wrapper = shallow(<SlotContainer {...testProps}/>);
+		// Edit empty
+		wrapper.find("#utorId2").simulate("change", {target: {value: "osbornharry31"}});
+		expect(wrapper.find("#utorId2").props().value).toEqual("osbornharry31");
+		// Edit occupied (should clear note)
+		wrapper.find("#utorId0").simulate("change", {target: {value: "osbornharry31"}});
+		expect(wrapper.find("#utorId0").props().value).toEqual("osbornharry31");
+		expect(wrapper.find("#note0").props().value).toEqual("");
+	});
+	
+	test("handleNoteChange changes note", () => {
+		const wrapper = shallow(<SlotContainer {...testProps}/>);
+		// Edit empty (shouldn't allow changes)
+		wrapper.find("#note2").simulate("change", {target: {value: "Honor is for fools."}});
+		expect(wrapper.find("#note2").props().value).toEqual("");
+		// Edit occupied
+		wrapper.find("#note0").simulate("change", {target: {value: "Honor is for fools."}});
+		expect(wrapper.find("#note0").props().value).toEqual("Honor is for fools.");
+	});
+	
+	test("handleEmpty empties slots", () => {
+		const wrapper = shallow(<SlotContainer {...testProps}/>);
+		wrapper.find("#empty-button").simulate("click");
+		const tree = renderer.create(<SlotContainer {...testProps}/>).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+});
+
+describe("student view", () => {
+	beforeEach(() => {
+		testSlots = [
+			{utorId: "parkerpeter15", note: "Everyone gets one."},
+			{utorId: "watsonmj25", note: "Face it Tiger... you just hit the jackpot!"},
+			{utorId: "", note: ""}
+		];
+		testProps = {
+			slots: testSlots.slice(),
+			startTime: moment("2077/01/01 00:00", "YYYY-MM-DD HH:mm"),
+			slotDuration: 600000,
+			isStudent: true,
+			user: "parkerpeter15"
+		};
+	});
+	
+	test("handleSlotClick assigns slot", () => {
+		const wrapper = shallow(<SlotContainer {...testProps}/>);
+		// Click on own slot (unregisters slot and deletes note)
+		wrapper.find("#slot0").props().onClick();
+		expect(wrapper.find("#utorId0").text()).toEqual("Available");
+		expect(wrapper.find("#note0").text()).toEqual("");
+		// Click on occupied slot (does nothing)
+		wrapper.find("#slot1").props().onClick();
+		expect(wrapper.find("#utorId1").text()).toEqual("Not Available");
+		expect(wrapper.find("#note1").text()).toEqual("");
+		// Click on empty slot (registers slot)
+		wrapper.find("#slot2").props().onClick();
+		
+		expect(wrapper.find("#utorId2").text()).toEqual("parkerpeter15");
+		expect(wrapper.find("#note2").props().value).toEqual("");
+	});
+	
+	test("handleUtorIdChange can't be called", () => {
+		const wrapper = shallow(<SlotContainer {...testProps}/>);
+		expect(wrapper.find("#utorId0").props().onClick).toEqual(undefined);
+	});
+	
+	test("handleNoteChange changes only own notes", () => {
+		const wrapper = shallow(<SlotContainer {...testProps}/>);
+		// Edit empty (shouldn't allow changes)
+		expect(wrapper.find("#note2").props().onChange).toEqual(undefined);
+		// Edit occupied (shouldn't allow changes)
+		expect(wrapper.find("#note1").props().onChange).toEqual(undefined);
+		// Edit own
+		wrapper.find("#note0").simulate("change", {target: {value: "With great powers comes great responsibility."}});
+		expect(wrapper.find("#note0").props().value).toEqual("With great powers comes great responsibility.");
+	});
+	
+	test("handleEmpty empties only own slots", () => {
+		const wrapper = shallow(<SlotContainer {...testProps}/>);
+		wrapper.find("#empty-button").simulate("click");
+		const tree = renderer.create(<SlotContainer {...testProps}/>).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+});
+
+
+/* let testProps, testSlots, mockChange, mockSubmit, mockClick;
 const testNote =
 	`My Bonnie lies over the ocean
 	My Bonnie lies over the sea
@@ -104,3 +220,4 @@ it("empties slots", () => {;
 		expect(e.find(`#note${i}`).props().value).toEqual("");
 	}
 });
+ */
