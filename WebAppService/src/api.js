348fcb272a06f6c0d1439a8ebd8d7b90e689ebf9
BC <bensonchan@users.noreply.github.com>
2018-11-08 12:00:20 -0500

    Add block and slot requests

--- WebAppService/src/api.js
+++ WebAppService/src/api.js
@@ -6,25 +6,53 @@ export default class Api {
 		this.sessionToken = "";
 	}
 	
+	authenticate(username, password) {
+		this.call("auth", "POST", {username: username, password: password});
+		this.sessionToken = "dummySessionToken123";
+		return {sessionToken: "dummySessionToken123"};
+	}
+	
+	getBlocks(utorId) {
+		return this.call(`blocks/${utorId}`, "GET", {});
+	}
+	getBlock(utorId, startTime) {
+		return this.call(`blocks/${utorId}/${startTime}`, "GET", {});
+	}
+	postBlock(utorId, startTime, block) {
+		return this.call(`blocks/${utorId}/${startTime}`, "POST", block);
+	}
+	deleteBlock(utorId, startTime, block) {
+		return this.call(`blocks/${utorId}/${startTime}`, "POST", block);
+	}
+	
+	getSlots(utorId, startTime) {
+		return this.call(`blocks/${utorId}/${startTime}/slots`, "GET", {});
+	}
+	getSlot(utorId, startTime, index) {
+		return this.call(`blocks/${utorId}/${startTime}/slots/${index}`, "GET", {});
+	}
+	postSlot(utorId, startTime, index, slot) {
+		return this.call(`blocks/${utorId}/${startTime}/slots/${index}`, "POST", slot);
+	}
+	
 	/**
-	* Makes an API call. When authenticating, session key returned will be 
-	* stored and used in other calls. Authenticate first before making other 
-	* API calls. Header is exclusively for authentication.
+	* Makes an API call. Authenticate first before making other 
+	* API calls. Header is exclusively for authenticating requests.
 	* @param {string} path The path to send the request to, appended to url. 
 	* @param {string} method HTTP request method.
 	* @param {object} body HTTP request body.
 	* @return {object} JSON response.
 	*/
 	call(path, method, body) {
-		if (path == "auth" || this.sessionToken != "") {
-			// Do API call.
-			console.log(`Successful ${method} to ${this.url}${path}.`);
-			
-			if (path == "auth" && method == "POST") {
-				this.sessionToken = "dummySessionToken123";
-				return {sessionToken: "dummySessionToken123"};
+		if (path == "auth" || this.sessionToken) {
+			let header = {};
+			if (path != "auth") {
+				header = {Authorization: `Basic ${this.sessionToken}`};
 			}
 			
+			// TODO: Do API call, throw error if unsuccessful.
+			
+			console.log(`Successful ${method} to ${this.url}${path}.`);
 			return {};
 		} else {
 			throw new UnauthorizedError(`Unauthorized ${method} to ${this.url}${path}.`);
