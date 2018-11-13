2c85de38d07fe4c0cebcc06b95b41e8aef4a948c
Asher Minden-Webb <amindenwebb@gmail.com>
2018-11-13 15:58:40 -0500

    [#21] Handle no auth case

--- APIService/api/auth/auth.py
+++ APIService/api/auth/auth.py
@@ -7,21 +7,30 @@ from flask import request
 from api import mongo
 
 
+def deny_authorization(reason=None):
+    content = {'status': 'Authorization denied.'}
+    if reason is not None:
+        content['reason'] = reason
+    return content, 401
+
+
 class Auth(Resource):
     # TODO: log request
     def get(self, id):
         # TODO: implement real auth
+        if request.authorization is None:
+            return deny_authorization("No credentials provided")
         password = request.authorization.password
         username = request.authorization.username
         if not self.check_password(username, password):
-            return {'status': 'Authorization denied.'}, 401
+            return deny_authorization("Invalid credentials")
         token = self.generate_token(id)
         # TODO: save id/token/expiry
-        return {'id': id, 'auth-token': token}, 200
+        return {'id': id, 'auth-token': token}
 
     @staticmethod
     def generate_token(id):
-        # TODO: 
+        # TODO: generate a real token?
         timestamp = datetime.utcnow()
         hash_string = bytes(id, "utf8") + bytes(str(timestamp), "utf8")
         return sha256(hash_string).hexdigest()
