61fd76698e05247ce4472651c785394e0fbaf661
Grant Wong <grant.hj.wong@gmail.com>
2018-11-14 15:51:19 -0500

    [#43, #45] DBService initialization script (squashed)
    Create initialization script for the service.
    
    Create mongoDB initialization script.

--- DBService/docker-scripts/docker-entrypoint.sh
+++ DBService/docker-scripts/docker-entrypoint.sh
@@ -2,12 +2,12 @@
 
 echo "[ DBSERVICE ENTRY POINT BEGIN ]"
 echo "Starting mongod instance..."
-mongod --port ${MONGO_PORT} --bind_ip ${MONGO_IP} --quiet &
+mongod --port ${MONGO_INIT_PORT} --quiet &
 sleep 2 # Really should be waiting for a return value, this will do for now.
 echo "mongod instance started!"
 echo "Creating EMPTY Mongo database ${MONGO_DBNAME} with validators..."
-mongo --eval "const ip = '${MONGO_IP}'; const port = '${MONGO_PORT}'; const dbName = '${MONGO_DBNAME}';" /docker-entrypoint-initdb.d/create_db_dev.js &&
+mongo ${MONGO_IP}:${MONGO_INIT_PORT}/${MONGO_DBNAME} /docker-entrypoint-initdb.d/create_db_dev.js
 echo "Mongo database ${MONGO_DBNAME} created!"
 echo "[ DBSERVICE ENTRY POINT END ]"
 mongod --shutdown
-exec mongod --port ${MONGO_PORT} --bind_ip ${MONGO_IP} --quiet
+exec mongod --port ${MONGO_SERVICE_PORT} --bind_ip_all --quiet
--- DBService/mongo-scripts/create_db_dev.js
+++ DBService/mongo-scripts/create_db_dev.js
@@ -1,6 +1,3 @@
-// Passed in environment const variables: ip, port, dbName
-db = connect(ip + ':' + port + '/' + dbName);
-
 load("/docker-entrypoint-initdb.d/identity_validator.js");
 load("/docker-entrypoint-initdb.d/courses_validator.js");
 load("/docker-entrypoint-initdb.d/blocks_validator.js");
