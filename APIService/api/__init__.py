93b47ae1bba346da61e0ed167b699e586698342d
Asher Minden-Webb <amindenwebb@gmail.com>
2018-11-20 19:39:42 -0500

    [#49] API integration testing fixture
    * Added a test fixture (TestCase abstract subclass) `IntegrationTest`
    * Added a test case for the auth endpoint - no database checking yet
    * Test fixtures should initialize a new database for each test

--- APIService/api/__init__.py
+++ APIService/api/__init__.py
@@ -11,30 +11,44 @@ def get_env_var(env_var_name, default):
     try:
         value = os.environ[env_var_name]
     except KeyError:
-        LOGGER.warning("%s missing, using default", env_var_name)
         value = default
+    LOGGER.info("Environment config:")
     LOGGER.info("%s=%s", env_var_name, value)
     return value
 
 
-def create_app(test_config=None):
-    LOGGER.info("=== Environment variables ===")
+def base_config():
     mongo_host = get_env_var("MONGO_HOST", "localhost")
     mongo_port = get_env_var("MONGO_PORT", "27017")
     mongo_db_name = get_env_var("MONGO_DBNAME", "dev_db")
+    return dict(
+        SECRET_KEY='dev',
+        MONGO_URI="mongodb://" + mongo_host + ":" + mongo_port,
+        DB_NAME=mongo_db_name
+    )
+
+
+def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        MONGO_URI="mongodb://" + mongo_host + ":" + mongo_port + "/" + mongo_db_name,
-    )
+    app.logger.setLevel(logging.INFO)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        try:
+            app.config.from_pyfile('config.py')
+        except FileNotFoundError:
+            app.logger.info("No config file found, loading config from environment variables")
+            app.config.from_mapping(
+                base_config()
+            )
     else:
         # load the test config if passed in
-        app.config.from_mapping(test_config)
+        config_mapping = base_config()
+        config_mapping.update(test_config)
+        app.logger.info("Config:")
+        app.logger.info(config_mapping)
+        app.config.from_mapping(**config_mapping)
 
     create_api(app)
 
